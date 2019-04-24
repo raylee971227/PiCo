6a7fd3ccee83558285d12a8a618732d9108df221
Raymond Lee <raymond@Raymonds-MacBook-Pro-2.local>
2019-04-24 17:34:38 -0400

    google log in user update redirect

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,25 +1,45 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-
+import history from '../history'
 /**
  * COMPONENT
  */
-export const UserHome = props => {
-  const {email} = props
-  return (
-    <div>
-      <h3>Welcome, {email}</h3>
-    </div>
-  )
+
+class UserHome extends Component {
+  componentDidMount() {
+    if(!this.props.user.accountSetUp) {
+      history.push('/updateuser');
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Welcome, {this.props.email}</h3>
+      </div>
+    )
+  }
 }
+
+// export const UserHome = props => {
+
+
+//   const {email} = props
+//   return (
+//     <div>
+//       <h3>Welcome, {email}</h3>
+//     </div>
+//   )
+// }
 
 /**
  * CONTAINER
  */
 const mapState = state => {
   return {
-    email: state.user.email
+    email: state.user.email,
+    user: state.user
   }
 }
 
