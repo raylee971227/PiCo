931e0d71b9fc3ea8eba72ae39c5b93dd3c6795b5
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2019-04-30 06:14:12 -0400

    Front end 30% done?

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -10,7 +10,34 @@ const AuthForm = props => {
   const {name, displayName, handleSubmit, error} = props
 
   return (
-   
+        <div>
+          <div className="description">
+              <img id="descimg" src="https://i.imgur.com/FfqPmEu.png"></img>
+              <hr></hr>
+              <p>
+              PICO
+
+              Minyoung Na / Ray Lee / Mrigank Saksena / Iris Huang  <br/>
+              Boilermaker <br/>
+              Good things come in pairs <br/>
+              Looking to mix up a backend with express/sequelize and a frontend with react/redux? That's boilermaker! <br/>
+              Follow along with the workshop to make your own! This canonical version can serve as a reference, or a starting point all on its own. <br/>
+              </p>
+              <p>
+              Setup
+              To use this boilerplate, you'll need to take the following steps:
+             
+              Don't fork or clone this repo! Instead, create a new, empty directory on your machine and git init (or create an empty repo on Github and clone it to your local machine) <br/>  <br/>
+              Run the following commands:  <br/>
+              git remote add boilermaker https://github.com/FullstackAcademy/boilermaker.git  <br/>
+              git fetch boilermaker  <br/> 
+              git merge boilermaker/master  <br/> <br/>
+              Why did we do that? Because every once in a while, boilermaker may be updated with additional features or bug fixes, and you can easily get those changes from now on by entering:
+             
+              git fetch boilermaker
+              git merge boilermaker/master
+              </p>
+          </div>
           <div className="authform">
             <form  onSubmit={handleSubmit} name={name}>
               <div>
@@ -28,9 +55,8 @@ const AuthForm = props => {
             </form>
             <hr></hr>
             <a href="/auth/google">{displayName} with Google</a>
-
+          </div>
        </div>
-     
   )
 }
 
